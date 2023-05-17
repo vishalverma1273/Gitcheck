e65a6949b5a8ed68e4e42ebe7d67ec9e3c988920
Vishal Verma <vishalverma1273@gmail.com>
2023-05-17 15:52:15 +0530

    Added implementation in Multiply.cpp

--- Multiply.cpp
+++ Multiply.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main() {
     int x,y;
-    cout<<"Enter Two Number"<<endl;
+    cout<<"Enter Two Number for Multiply"<<endl;
     cin>>x >> y;
     cout<< x*y <<endl;
     return 0;
